e70ac838d8222b1cf5df6b9a92e9223cc140f818
Divya <divyakanabar9@gmail.com>
2023-12-11 14:55:44 -0800

    Created a form in GoalForm.jsx - take an input/goal, added button to submit

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -2,10 +2,25 @@ import { useState } from "react"
 import { UseSelector, useDispatch } from "react-redux"
 
 function GoalForm() {
+
+    const [text, setText] = useState('')
+
+    const onSubmit = e => {
+        e.preventDefault()
+    }
+
   return (
-    <div>
-        Goal Form
-    </div>
+    <section className="form">
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label htmlFor="text">Goal</label>
+                <input type="text" name="text" id="text" value={text} onChange={(e) => setText(e.target.value)}/>
+            </div>
+            <div className="form-group">
+                <button className="btn btn-block" type="submit">Add Goal</button>
+            </div>
+        </form>
+    </section>
   )
 }
 
