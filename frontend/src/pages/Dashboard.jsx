688c85e7eee5824c128497bfa42615475734f5da
Divya <divyakanabar9@gmail.com>
2023-12-11 15:44:32 -0800

    Added dispatch to Dashboard.js to getGoals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import Spinner from '../components/Spinner'
+import { getGoals } from '../features/goals/goalSlice'
 
 function Dashboard() {
 
   const navigate = useNavigate()
 
+  const dispatch = useDispatch()
+
   const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
 
   useEffect(() => {
+
+    if(isError){
+      console.log(message);
+    }
+
     if(!user){
       navigate('/login')
     }
+
+    dispatch(getGoals())
   }, [user, navigate])
 
   return (
