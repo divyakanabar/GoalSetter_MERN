444cd3677fbdb8eab35e10691b1b035ab114f0a2
Divya <divyakanabar9@gmail.com>
2023-12-11 15:18:08 -0800

    Updated goalService & goalSlice to add a new goal after authorization

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
-import { UseSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
+import {createGoal} from '../features/goals/goalSlice'
 
 function GoalForm() {
 
     const [text, setText] = useState('')
 
+    const dispatch = useDispatch()
+
     const onSubmit = e => {
         e.preventDefault()
+
+        dispatch(createGoal({text}))
+        setText('')
     }
 
   return (
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { UseSelector, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import GoalForm from '../components/GoalForm'
 
 function Dashboard() {
